b11234b7b3ba51db6adb379bc9105012350aecd2
MUMULYNMU <892697736@qq.com>
2020-09-16 14:02:49 +0800

    0916

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
     <h1>site : {{site}}</h1>
     <h1>url : {{url}}</h1>
     <h1>{{details()}}</h1>
-    <h1>{{details()}}</h1>
+    
 </div>
 <script type="text/javascript">
     var vm = new Vue({
